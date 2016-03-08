2d0b8936899ac687cdba1ea5061e7292e4cc3c8c
ks <ks@ks.ks>
2016-03-08 12:13:55 +0800

    alot changes

---
+++ PenYourPrayerServer/Models/Struct/PrayerRequest.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using PenYourPrayerServer.Supporting.HMAC;
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace PenYourPrayerServer.Models.Struct
+{
+    public class PrayerRequest
+    {
+
+        public string PrayerRequestID { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public bool Answered { get; set; }
+        public string AnswerComment { get; set; }
+
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime AnsweredWhen;
+
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime CreatedWhen;
+
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime TouchedWhen;
+
+        public List<PrayerRequestAttachment> attachments { get; set; }
+
+        public static explicit operator PrayerRequest(usp_GetLatestPrayerRequestResult c)
+        {
+            PrayerRequest pr = new PrayerRequest();
+            
+            pr.PrayerRequestID = c.PrayerRequestID.ToString();
+            pr.Subject = c.Subject;
+            pr.Description = c.Description;
+            pr.CreatedWhen = c.CreatedWhen;
+            pr.TouchedWhen = c.TouchedWhen;
+            pr.AnswerComment = c.AnswerComment;
+            pr.Answered = c.Answered;
+            if (pr.AnsweredWhen != null && pr.Answered)
+            {
+                pr.AnsweredWhen = (DateTime)c.AnsweredWhen;
+            }
+            return pr;
+        }
+    }
+
+    [XmlRootAttribute("AllPrayerRequest")]
+    public class AllPrayerRequest
+    {
+        [XmlElement("PrayerRequest")]
+        public List<PrayerRequest> prayerRequest { get; set; }
+    }
+}
---
+++ PenYourPrayerServer/Models/Struct/PrayerRequestAttachment.cs
@@ -0,0 +1,30 @@
+﻿using PenYourPrayerServer.Supporting.HMAC;
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+
+namespace PenYourPrayerServer.Models.Struct
+{
+    public class PrayerRequestAttachment
+    {
+        public long AttachmentID { get; set; }
+        public long PrayerRequestID { get; set; }
+        public string GUID { get; set; }
+        public string OriginalFilePath { get; set; }
+        public string URLPath { get; set; }
+        public string UserID { get; set; }
+        public string FileName { get; set; }
+        
+    }
+
+
+
+
+    [XmlRootAttribute("AllAttachments")]
+    public class AllPrayerRequestAttachment
+    {
+        [XmlElement("Attachment")]
+        public List<PrayerRequestAttachment> attachments { get; set; }
+    }
+}
